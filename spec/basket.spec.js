4f1952dd9550d54cb5cba7927be23cf46b24af94
Ben Clayton <benclayton1@hotmail.com>
2021-06-30 15:49:54 +0100

    second test successful, the bagel boys are in business

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,7 +1,7 @@
 const assertEquals = require('./../test-framework.js')
 const Basket = require('./../src/basket.js')
 
-let basket, item, result
+let basket, item, result, result2, expectedResult
 
 // Try to add one item to basket
 
@@ -14,3 +14,23 @@ result = basket.add(item)
 
 // Verify
 console.log(assertEquals(item, result))
+
+// try to add two items to basket 
+console.log('\nTest to add two items: ')
+// Setup
+basket = new Basket()
+item = 'ham and cheese bagel'
+seconditem = 'tuna bagel'
+expectedResult = 2
+
+// Execute
+basket.add(item)
+basket.add(seconditem)
+result = basket.items.length
+
+// Verify
+console.log(assertEquals(result, expectedResult))
+
+// try to remove 
+
+
---
+++ specRunner.js
@@ -0,0 +1,1 @@
+require ('./spec/basket.spec.js')
